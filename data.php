47321eba7b98b7636ab5c7467ede206bd98fcb4e
Carlos Guarda <carlos.guarda@qbitla.com>
2022-06-21 18:32:40 -0500

    error_no_styles_corregido

--- data.php
+++ data.php
@@ -32,5 +32,3 @@ $products = array(
         "img" => "JuegoLlaves.jpg"
     )
 );
-
- ?>
--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
 session_start();
 error_reporting(E_ERROR | E_PARSE);
-  include "data.php";
-  include "templates.php";
-  #echo $header_html;
-  #sólo se muestra el carrito si hay productos
-  if(isset($_SESSION["cart"]) && count($_SESSION["cart"]) > 0){
- ?>
+include "data.php";
+include "templates.php";
+echo $header_html;
+#sólo se muestra el carrito si hay productos
+if (isset($_SESSION["cart"]) && count($_SESSION["cart"]) > 0) {
+?>
   <div class="container">
     <div class="row">
       <div class="card">
-       <div class="card-body">
-         <h5 class="card-title">Carrito de compras</h5>
-         <p class="card-text">Lista de productos seleccionados.
+        <div class="card-body">
+          <h5 class="card-title">Carrito de compras</h5>
+          <p class="card-text">Lista de productos seleccionados.
           <table class="table table-condensed">
             <thead>
               <th>Código</th>
@@ -23,34 +23,35 @@ error_reporting(E_ERROR | E_PARSE);
               <th>&nbsp;</th>
             </thead>
             <tbody>
-           <?php
-           $total = 0;
-           foreach ($_SESSION["cart"] as $key => $value) {
-             # se calcula el subtotal y se va aumentando el total en el bucle
-              $subtotal = $products[$key]["price"]*$value["qty"];
-              $total += $subtotal;
-             ?>
+              <?php
+              $total = 0;
+              foreach ($_SESSION["cart"] as $key => $value) {
+                # se calcula el subtotal y se va aumentando el total en el bucle
+                $subtotal = $products[$key]["price"] * $value["qty"];
+                $total += $subtotal;
+              ?>
 
-             <tr>
-               <td><?php echo $products[$key]["id"] ?></td>
-               <td><?php echo $products[$key]["name"] ?></td>
-               <td style="text-align: right;"><?php echo $value["qty"] ?></td>
-               <td style="text-align: right;"><?php echo $products[$key]["price"] ?></td>
-               <td style="text-align: right;"><?php echo $subtotal ?></td>
-               <td>
-                 <a href="remove_from_cart.php?id=<?php echo $key ?>" title="Disminuir"><i class="fa fa-minus" aria-hidden="true"></i></a>&nbsp;&nbsp;
-                 <a href="remove_from_cart.php?removeAll=1&id=<?php echo $key ?>" title="Eliminar"><i class="fa fa-trash" aria-hidden="true"></i></a>
-               </td>
-             </tr>
-          <?php }} ?>
-          <tfoot>
-            <tr>
-              <td colspan="5" style="text-align:right;">
-                TOTAL : <?php echo $total ?>
-              </td>
-            </tr>
-          </tfoot>
-          </tbody>
+                <tr>
+                  <td><?php echo $products[$key]["id"] ?></td>
+                  <td><?php echo $products[$key]["name"] ?></td>
+                  <td style="text-align: right;"><?php echo $value["qty"] ?></td>
+                  <td style="text-align: right;"><?php echo $products[$key]["price"] ?></td>
+                  <td style="text-align: right;"><?php echo $subtotal ?></td>
+                  <td>
+                    <a href="remove_from_cart.php?id=<?php echo $key ?>" title="Disminuir"><i class="fa fa-minus" aria-hidden="true"></i></a>&nbsp;&nbsp;
+                    <a href="remove_from_cart.php?removeAll=1&id=<?php echo $key ?>" title="Eliminar"><i class="fa fa-trash" aria-hidden="true"></i></a>
+                  </td>
+                </tr>
+            <?php }
+            } ?>
+            <tfoot>
+              <tr>
+                <td colspan="5" style="text-align:right;">
+                  TOTAL : <?php echo $total ?>
+                </td>
+              </tr>
+            </tfoot>
+            </tbody>
           </table>
         </div>
       </div>
@@ -58,29 +59,29 @@ error_reporting(E_ERROR | E_PARSE);
   </div>
 
 
- <div class="card">
-   <div class="card-body">
-     <h5 class="card-title">Catálogo de productos</h5>
-     <p class="card-text">Lista de productos disponibles para la venta.
-       <div class="container">
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Catálogo de productos</h5>
+      <p class="card-text">Lista de productos disponibles para la venta.
+      <div class="container">
         <div class="row">
           <?php foreach ($products as $key => $value) { ?>
             <div class="col">
-             <div class="card">
-               <img src="<?php echo './images/'.$value["img"] ?>" class="card-img-top">
-               <div class="card-body">
-                 <h5 class="card-title"><?php echo $value["name"].' (S/ '.$value["price"].') '?></h5>
-                 <p class="card-text"><?php echo $value["description"] ?></p>
-                 <a href="./add_to_cart.php?id=<?php echo $key ?>" class="btn btn-primary">Añadir</a>
-               </div>
+              <div class="card">
+                <img src="<?php echo './images/' . $value["img"] ?>" class="card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title"><?php echo $value["name"] . ' (S/ ' . $value["price"] . ') ' ?></h5>
+                  <p class="card-text"><?php echo $value["description"] ?></p>
+                  <a href="./add_to_cart.php?id=<?php echo $key ?>" class="btn btn-primary">Añadir</a>
+                </div>
               </div>
             </div>
           <?php } ?>
         </div>
       </div>
-    </p>
-   </div>
- </div>
-<?php
-echo $footer_html
- ?>
+      </p>
+    </div>
+  </div>
+  <?php
+  echo $footer_html
+  ?>

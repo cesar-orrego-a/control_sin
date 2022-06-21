37cc2e8137a0f8ff63cc07c0eabd82c8bae928cd
DanielaBayonaSeminario <daniela.bayona@alum.udep.edu.pe>
2022-06-21 18:52:14 -0500

    error_no_styles

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+</head>
+<body>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" >
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" ></script>
+        <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/styles.css">
+</body>
+</html>
 <?php
 session_start();
 error_reporting(E_ERROR | E_PARSE);

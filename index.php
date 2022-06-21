ef59448c7be782fe707ce041c7c4c2f62818b98e
Daniel Cobos Sánchez <daniel.cobos@alum.udep.edu.pe>
2022-06-21 18:42:21 -0500

    Visualizar imagenes

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ error_reporting(E_ERROR | E_PARSE);
           <?php foreach ($products as $key => $value) { ?>
             <div class="col">
              <div class="card">
-               <img src="<?php echo './images/'.$value["img"] ?>" class="card-img-top">
+               <img src="<?php echo 'products_images/'.$value["img"] ?>" class="card-img-top">
                <div class="card-body">
                  <h5 class="card-title"><?php echo $value["name"].' (S/ '.$value["price"].') '?></h5>
                  <p class="card-text"><?php echo $value["description"] ?></p>

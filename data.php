d4fbc12b10887569fa70cd2c5344ffd3030d7712
fernando <fernando.medinacoronado25@gmail.com>
2022-06-21 18:58:32 -0500

    solucion_(error_no_show_images)

--- data.php
+++ data.php
@@ -1,35 +1,35 @@
 <?php
 
 $products = array(
-    "DEDS" => array(
-        "name" => "Destornillador plano 7mm",
-        "description" => "Destornillador plano 7mm, mango de goma, punta imantada.",
-        "price" => "15.6",
-        "img" => "DestornilladorPlano.jpg"
+    $DEDS => array(
+        $name => "Destornillador plano 7mm",
+        $description => "Destornillador plano 7mm, mango de goma, punta imantada.",
+        $price => "15.6",
+        $img => "DestornilladorPlano.jpg"
     ),
-    "DEXT" => array(
-        "name" => "Destornillador estrella 7mm",
-        "description" => "Destornillador estrella 7mm, mango de goma, punta imantada.",
-        "price" => "14.5",
-        "img" => "DestornilladorEstrella.jpg"
+    $DEXT => array(
+        $name => "Destornillador estrella 7mm",
+        $description => "Destornillador estrella 7mm, mango de goma, punta imantada.",
+        $price => "14.5",
+        $img => "DestornilladorEstrella.jpg"
     ),
-    "DATS" => array(
-        "name" => "Dado de ajuste continuo, pequeño",
-        "description" => "Dado de ajuste T, continuo, pequeño",
-        "price" => "18.0",
-        "img" => "DadoContinuo.jpg"
+    $DATS => array(
+        $name => "Dado de ajuste continuo, pequeño",
+        $description => "Dado de ajuste T, continuo, pequeño",
+        $price => "18.0",
+        $img => "DadoContinuo.jpg"
     ),
-    "HAMM" => array(
-        "name" => "Martillo mango de madera, 10 inch",
-        "description" => "Martillo mango de madera de 10 pulgadas",
-        "price" => "40.5",
-        "img" => "MartilloMadera.jpg"
+    $HAMM => array(
+        $name => "Martillo mango de madera, 10 inch",
+        $description => "Martillo mango de madera de 10 pulgadas",
+        $price => "40.5",
+        $img => "MartilloMadera.jpg"
     ),
-    "KEYS" => array(
-        "name" => "Juego de llaves combinadas 5/16 inch",
-        "description" => "Juego de 31 llaves combinadas de 5/16 pulgadas",
-        "price" => "27.9",
-        "img" => "JuegoLlaves.jpg"
+    $KEYS => array(
+        $name => "Juego de llaves combinadas 5/16 inch",
+        $description => "Juego de 31 llaves combinadas de 5/16 pulgadas",
+        $price => "27.9",
+        $img => "JuegoLlaves.jpg"
     )
 );
 

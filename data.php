67210412f9093266269668d492a4b483264132e5
LuchoASH <luis.sanchez.h@alum.udep.edu.pe>
2022-06-21 18:53:25 -0500

    Corrección del ID

--- data.php
+++ data.php
@@ -6,30 +6,35 @@ $products = array(
         "description" => "Destornillador plano 7mm, mango de goma, punta imantada.",
         "price" => "15.6",
         "img" => "DestornilladorPlano.jpg"
+        "id" => "1",
     ),
     "DEXT" => array(
         "name" => "Destornillador estrella 7mm",
         "description" => "Destornillador estrella 7mm, mango de goma, punta imantada.",
         "price" => "14.5",
         "img" => "DestornilladorEstrella.jpg"
+        "id" => "2",
     ),
     "DATS" => array(
         "name" => "Dado de ajuste continuo, pequeño",
         "description" => "Dado de ajuste T, continuo, pequeño",
         "price" => "18.0",
         "img" => "DadoContinuo.jpg"
+        "id" => "3",
     ),
     "HAMM" => array(
         "name" => "Martillo mango de madera, 10 inch",
         "description" => "Martillo mango de madera de 10 pulgadas",
         "price" => "40.5",
         "img" => "products_images/MartilloMadera.jpg"
+        "id" => "4",
     ),
     "KEYS" => array(
         "name" => "Juego de llaves combinadas 5/16 inch",
         "description" => "Juego de 31 llaves combinadas de 5/16 pulgadas",
         "price" => "27.9",
         "img" => "JuegoLlaves.jpg"
+        "id" => "5",
     )
 );
 

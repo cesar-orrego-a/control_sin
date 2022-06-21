a70755fdb351268e9eed3cee1298888ee33fc942
Cristhian Daniel Calle Vega <cristhian.calle@alum.udep.edu.pe>
2022-06-21 18:47:43 -0500

    se ha corregido el problema para eliminar

--- remove_from_cart.php
+++ remove_from_cart.php
@@ -5,10 +5,12 @@ if(isset($_SESSION["cart"][$_GET["id"]])){
   if(isset($_GET["remove_all"])){
     unset($_SESSION["cart"][$_GET["id"]]);
   }else{
-    $_SESSION["cart"][$_GET["id"]]["qty"]--;
-  }
-  if($_SESSION["cart"][$_GET["id"]]["qty"] <1){
-    unset($_SESSION["cart"][$_GET["id"]]);
+    if ($_SESSION["cart"][$_GET["id"]]["qty"]>1){
+      $_SESSION["cart"][$_GET["id"]]["qty"]--;
+    }
+    else {
+      unset($_SESSION["cart"][$_GET["id"]]);
+    }
   }
 }
 $_SESSION["alert"] = 'Producto eliminado del carrito correctamente. ';

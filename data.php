a6b489d7c35c5dae2eaa2e8edd1a1b2a1b56a1f3
Cesar Orrego A <orrego.cesar@gmail.com>
2022-06-21 00:11:24 -0500

    se añaden las imágenes para el catálogo

--- data.php
+++ data.php
@@ -4,39 +4,33 @@ $products = array(
     "DEDS" => array(
         "name" => "Destornillador plano 7mm",
         "description" => "Destornillador plano 7mm, mango de goma, punta imantada.",
-        "price" => "15.6"
+        "price" => "15.6",
+        "img" => "DestornilladorPlano.jpg"
     ),
     "DEXT" => array(
         "name" => "Destornillador estrella 7mm",
         "description" => "Destornillador estrella 7mm, mango de goma, punta imantada.",
-        "price" => "14.5"
+        "price" => "14.5",
+        "img" => "DestornilladorEstrella.jpg"
     ),
     "DATS" => array(
         "name" => "Dado de ajuste continuo, pequeño",
         "description" => "Dado de ajuste T, continuo, pequeño",
-        "price" => "18.0"
+        "price" => "18.0",
+        "img" => "DadoContinuo.jpg"
     ),
     "HAMM" => array(
         "name" => "Martillo mango de madera, 10 inch",
         "description" => "Martillo mango de madera de 10 pulgadas",
-        "price" => "40.5"
+        "price" => "40.5",
+        "img" => "MartilloMadera.jpg"
     ),
-    "TRNO" => array(
-        "name" => "Torno cabezal universal",
-        "description" => "Torno de cabezal universal",
-        "price" => "27.9"
+    "KEYS" => array(
+        "name" => "Juego de llaves combinadas 5/16 inch",
+        "description" => "Juego de 31 llaves combinadas de 5/16 pulgadas",
+        "price" => "27.9",
+        "img" => "JuegoLlaves.jpg"
     )
 );
-
-foreach ($products as $key => $value) {
-  echo "key: ".$key;
-  echo "<br>";
-  echo "name: ".$value["name"];
-  echo "<br>";
-  echo "description: ".$value["description"];
-  echo "<br>";
-  echo "price: ".$value["price"];
-  echo "<hr>";
-}
 
  ?>

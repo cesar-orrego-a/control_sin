a62e831def64a635f42d9ca218d465eb1e4c5d1b
Daniel Cobos Sánchez <daniel.cobos@alum.udep.edu.pe>
2022-06-21 19:04:20 -0500

    borrar carrito

--- remove_from_cart.php
+++ remove_from_cart.php
@@ -5,9 +5,6 @@ if(isset($_SESSION["cart"][$_GET["id"]])){
   if(isset($_GET["remove_all"])){
     unset($_SESSION["cart"][$_GET["id"]]);
   }else{
-    $_SESSION["cart"][$_GET["id"]]["qty"]--;
-  }
-  if($_SESSION["cart"][$_GET["id"]]["qty"] <1){
     unset($_SESSION["cart"][$_GET["id"]]);
   }
 }

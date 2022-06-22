dad5ee0126fddc487414eebc25fdf65bc5b0004a
joseolaechea <jose.olaechea@alum.udep.edu.pe>
2022-06-21 19:08:55 -0500

    error_no_showproduct_id

--- data.php
+++ data.php
@@ -2,35 +2,35 @@
 
 $products = array(
     "DEDS" => array(
-        "id" => "1",
+        "id" => "01",
         "name" => "Destornillador plano 7mm",
         "description" => "Destornillador plano 7mm, mango de goma, punta imantada.",
         "price" => "15.6",
         "img" => "DestornilladorPlano.jpg"
     ),
     "DEXT" => array(
-        "id" => "2",
+        "id" => "02",
         "name" => "Destornillador estrella 7mm",
         "description" => "Destornillador estrella 7mm, mango de goma, punta imantada.",
         "price" => "14.5",
         "img" => "DestornilladorEstrella.jpg"
     ),
     "DATS" => array(
-        "id" => "3",
+        "id" => "03",
         "name" => "Dado de ajuste continuo, pequeño",
         "description" => "Dado de ajuste T, continuo, pequeño",
         "price" => "18.0",
         "img" => "DadoContinuo.jpg"
     ),
     "HAMM" => array(
-        "id" => "4",
+        "id" => "04",
         "name" => "Martillo mango de madera, 10 inch",
         "description" => "Martillo mango de madera de 10 pulgadas",
         "price" => "40.5",
         "img" => "MartilloMadera.jpg"
     ),
     "KEYS" => array(
-        "id" => "5",
+        "id" => "05",
         "name" => "Juego de llaves combinadas 5/16 inch",
         "description" => "Juego de 31 llaves combinadas de 5/16 pulgadas",
         "price" => "27.9",
